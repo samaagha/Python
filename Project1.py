f8d753805c70917b44da39d837be393574d278fd
samaagha <sama.aagha14@gmail.com>
2026-08-30 18:38:12 +0200

    Add files via upload

---
+++ Project1.py
@@ -0,0 +1,5 @@
+#a welcome message
+print("Welcome to the youtube Channel Name Generator!")
+name = input("What is your nickname? \n")
+subject = input("What is your Channel about? \n")
+print("You could name your Channel " + "(" + (subject) + " " + "with" +  " " + (name) + ")")
