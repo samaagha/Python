1e37e2ebd637a22ed95f419739c15300780604d6
samaagha <sama.aagha14@gmail.com>
2026-08-30 19:03:33 +0200

    Uploud first Python project
    project1 ( YouTube Channel Name Generator): A  Simple Python CLI ( Command Line Interface) script that generates potential Youtube channel names by interacting with the user and combining string inputs (input (), print(), and  string concatenation).

---
+++ Project1.py
@@ -0,0 +1,4 @@
+print("Welcome to the youtube Channel Name Generator!")
+name = input("What is your nickname? \n")
+subject = input("What is your Channel about? \n")
+print("You could name your Channel " + "(" + (subject) + " " + "with" +  " " + (name) + ")")
